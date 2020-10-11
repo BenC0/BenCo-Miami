5244633ae8ac5d86645580064e44333dd5ac35a7
BenC0 <suddensquirrel94@gmail.com>
2020-10-11 12:48:47 +0100

    cleared it all

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './styles/style.scss';
-import navigation from "./scripts/navigation"
-
-function init() {
-	document.body.classList.add('loaded')
-	navigation.init()
-}
-
-document.addEventListener("DOMContentLoaded", init);
