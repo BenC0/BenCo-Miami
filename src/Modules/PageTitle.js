63008a2e49f7ed8e5f9e6c08368bd9cb0212e918
BenC0 <suddensquirrel94@gmail.com>
2020-10-21 16:48:08 +0100

    playing around with page title designs

--- src/Modules/PageTitle.js
+++ src/Modules/PageTitle.js
@@ -1,16 +1,51 @@
 import "./PageTitle.css"
 import React from 'react';
 
-function PageTitle(props) {
-	return [
-		<div id="pageTitle" className="content m-span-4 d-span-12">
-			<div className="title_container">
-				<div className="background">
+import PalmTree from "./assets/Palm-Tree-SVG"
+
+export function Shapes(props) {
+	console.log(props)
+	let Shapes = <div className="background"></div>
+	switch (props.title) {
+		case "Benji Cohen":
+			Shapes = [
+						<div className="background">
+							<div className="shape"></div>
+							<div className="shape"></div>
+							<div className="shape"></div>
+							<div className="circle"></div>
+							<div className="palm-tree right"> <PalmTree /> </div>
+						</div>,
+						<div className="background foreground">
+							<div className="shape"></div>
+							<div className="shape"></div>
+							<div className="shape"></div>
+						</div>
+					]
+			break;
+		default:
+			Shapes = <div className="background">
+					<div className="shape"></div>
+					<div className="shape"></div>
+					<div className="shape"></div>
+					<div className="shape"></div>
+					<div className="shape"></div>
+					<div className="shape"></div>
 					<div className="shape"></div>
 					<div className="shape"></div>
 					<div className="shape"></div>
 					<div className="shape"></div>
 				</div>
+			break;
+	}
+	return Shapes
+} 
+
+function PageTitle(props) {
+	return [
+		<div id="pageTitle" className="content m-span-4 d-span-12">
+			<div className="title_container">
+				<Shapes title={props.title} />
 				<h1 className="title">{props.title}</h1>
 			</div>
 		</div>
